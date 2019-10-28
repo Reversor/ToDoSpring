3034efc649eadbad4c408988582bba29f9aefc4f
Rost Zhiharev <revers914@gmail.com>
2019-10-28 14:09:46 +0400

    wip

--- src/main/java/io/github/reversor/todo/controller/ToDoController.java
+++ src/main/java/io/github/reversor/todo/controller/ToDoController.java
@@ -1,9 +1,25 @@
 package io.github.reversor.todo.controller;
 
+import io.github.reversor.todo.dao.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("test")
 public class ToDoController {
 
+    private ToDoRepository toDoRepository;
+
+    @Autowired
+    public ToDoController(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+    @GetMapping("foo")
+    public String foo() {
+        return "foo";
+    }
 
 }
--- src/main/java/io/github/reversor/todo/dao/ToDoRepository.java
+++ src/main/java/io/github/reversor/todo/dao/ToDoRepository.java
@@ -1,11 +1,13 @@
 package io.github.reversor.todo.dao;
 
 import io.github.reversor.todo.entity.ToDo;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
     @Override
     Iterable<ToDo> findAll();
 
-    Iterable<ToDo> getAllByStartTimeNotContaining();
+    Iterable<ToDo> getAllByStartTimeNotContaining(OffsetDateTime dateTime);
 }
